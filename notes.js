bfd67de19b69d2a94d9d89c62fe5f187f51e97b3
Maurik <Ferry.van.Maurik@nike.com>
2017-04-14 17:32:33 +0200

    Listing Notes

--- notes.js
+++ notes.js
@@ -1,5 +1,3 @@
-console.log('Starting notes.js');
-
 const fs = require('fs');
 
 var fetchNotes = () => {
@@ -34,7 +32,7 @@ var addNote = (title, body) => {
 };
 
 var getAll = () => {
-    console.log('Getting all notes');
+    return fetchNotes();
 };
 
 var getNote = (title) => {
@@ -51,7 +49,7 @@ var removeNote = (title) => {
     return notes.length !== filteredNotes.length;
 };
 
-debugger;
+//debugger;
 var logNote = (note) => {
         console.log('---');
         console.log('Title: ' + note.title);
